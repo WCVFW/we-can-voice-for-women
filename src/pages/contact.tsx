c1c860a61efd4aaea6a242f8246a3d7c617b39bb
WCVFW <wcvfw2019@gmail.com>
2025-07-09 12:22:48 +0530

    updated 9

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -12,7 +12,7 @@ export default function Contact() {
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://16.171.19.4:9090/api/contact', {
+      const res = await fetch('https://wecanvoiceforwomen.org/api/contact', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
